35e537452ea4469bf2fe9456a00fe5aeee3920e3
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-03-05 20:12:26 +0000

    created function to generation manager profile

--- index.js
+++ index.js
@@ -12,4 +12,41 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+const idList = []
+const teamMembers = []
 
+const appMenu = () => {
+    // Function to generate prompts for manager's profile
+    function createManager(){
+        console.log("Please build your team.");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is the team manager's name?",
+                validate: answer => {
+                    if(answer !== ""){
+                        return true
+                    }
+                    return "Please enter a name."
+                }
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the team manager's id?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the team manager's email?"
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is the team manager's office number?"
+            }
+
+        ])
+    }
+}
