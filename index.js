df350479450730904350ddbc36395d54bd1dfabf
Alfred William Nunoo <alfred.wnunoo@gmail.com>
2023-03-06 11:51:46 +0000

    added functions to generate promts to build engineer profile

--- index.js
+++ index.js
@@ -11,11 +11,80 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Code to gather information about the development team members, and render the HTML file.
 const idList = []
 const teamMembers = []
 
 const appMenu = () => {
+
+    function buildTeam() {
+
+    }
+
+    function addIntern() {
+
+    }
+
+    // Function to generate prompts for engineer's profile
+    function addEngineer() {
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "Add engineer name",
+            },
+            {
+                type: "input",
+                name: "engineerId",
+                message: "Add engineer id",
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "Add engineer email",
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "Add engineer github",
+            },
+            
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerId, answers.engineerEmail, answers.engineerGithub);
+            teamMembers.push(engineer);
+            idList.push(answers.engineerId);
+            console.log(engineer);
+            createTeam()
+        })
+    }
+    
+
+    // Function to create team 
+    function createTeam() {
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "Add preferred team member",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "No more team member to add"
+                ]
+            }
+        ]).then(userChoice => {
+            if(userChoice.memberChoice === "Engineer") {
+                addEngineer();
+            } else if(userChoice.memberChoice === "Intern") {
+                addIntern();
+            } else {
+                buildTeam();
+            }
+
+        })
+    }
+
+
     // Function to generate prompts for manager's profile
     function createManager(){
         console.log("Please build your team.");
@@ -49,8 +118,10 @@ const appMenu = () => {
 
         ]).then(answers => {
             const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+            console.log(manager);
             teamMembers.push(manager);
             idList.push(answers.managerId);
+            createTeam();
         })
     }
     createManager();
